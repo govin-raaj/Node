109a9743c7a405ee21c1481789b400dd546ea6a7
govin-raaj <govin30raaj@gmail.com>
2026-01-14 19:19:44 +0530

    m1 v10

--- M1/index.js
+++ M1/index.js
@@ -1,8 +1,21 @@
 const fs=require('fs');
-const text=fs.readFileSync('M1/input.txt','utf-8');
-console.log(text);
 
-const textout='this is what we found in the file '+text;
-fs.writeFileSync('M1/output.txt',textout);
-console.log('Output written to M1/output.txt');
+//sync
+// const text=fs.readFileSync('M1/input.txt','utf-8');
+// console.log(text);
 
+// const textout='this is what we found in the file '+text;
+// fs.writeFileSync('M1/output.txt',textout);
+// console.log('Output written to M1/output.txt');
+
+//async
+
+fs.readFile('M1/file.txt','utf-8', (err, data)=> {
+    if (err) return  console.log("Error");
+
+    fs.readFile(`M1/${data}.txt`,'utf-8', (err , data2)=>{
+        console.log(data2);
+    });
+});
+
+console.log("reading file.....")
