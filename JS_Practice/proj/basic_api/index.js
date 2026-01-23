f28162f973e89dcdb810be930ccc79917eff9fd4
govin-raaj <govin30raaj@gmail.com>
2026-01-23 17:32:13 +0530

    done

--- JS_Practice/proj/basic_api/index.js
+++ JS_Practice/proj/basic_api/index.js
@@ -18,32 +18,26 @@ fetch('https://dummyjson.com/products')
 
         function printData(products) {
             products.map(product => { console.log(`Product name: ${product.title} , Price:$${product.price}, Ratings: ${product.rating}, stock:${product.stock}`) })
-
         }
 
         function fltr(products) {
         const filteredProducts = products.filter(product => product.price > 100);
-
         console.log(
             "Filtered Products (price > 100):",
             filteredProducts.map(p => p.title)
         );
-
         return filteredProducts;
         }
 
-
         function totalPrice(products){
             let total=products.reduce((acc,product)=>acc+product.price,0);
-            console.log("Total Price of all products: $",total);
+            console.log("Total Price of products: $",total);
             return total;
         }
 
         printData(products);
-        console.log(fltr(products).length);
-        totalPrice(fltr(products));
-        
-
-
+        let filteredProducts=fltr(products);
+        console.log(filteredProducts.length);
+        totalPrice(filteredProducts);
     })
     .catch(err => console.error("Error:", err));
